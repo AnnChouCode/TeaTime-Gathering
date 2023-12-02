1d7249cb6a628d5b55e0c914563527f17df71a75
Ann Chou <yuanchou.ann@gmail.com>
2023-12-02 18:14:08 +0800

    chore：更新 js 樣板存放路徑

--- assets/js/isLoggedIn.js
+++
@@ -1,9 +0,0 @@
-function isLoggedIn(_token){
-    //如果 token 內無值，則返回未登入
-    if (!_token){return false}
-
-    //如果 token 內有值，則返回登入
-    return true
-}
-
-export default isLoggedIn;
--- assets/js/isPastEvent.js
+++
@@ -1,9 +0,0 @@
-//判斷傳入時間是否早於今天（意即過去式事件）
-function isPastEvent(time) {
-    const eventDate = moment(time, 'YYYY/MM/DD HH:mm');
-    const result = moment().isAfter(eventDate)
-
-    return result
-  }
-
-export default isPastEvent;
