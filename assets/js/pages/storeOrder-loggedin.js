3803d20cd5c95daa358e8febd84fca3316034e7a
Ann Chou <yuanchou.ann@gmail.com>
2023-12-05 11:26:20 +0800

    feat：新增按鈕登入判斷 1. 「購物車」、「建立揪團」、「寫評價」，共計 5 個按鈕，新增帳號登入判斷 未登入狀態點擊按鈕，將提示需登入，已登入狀態則進入後續功能

---
+++ assets/js/pages/storeOrder-loggedin.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import * as bootstrap from "bootstrap/dist/js/bootstrap.min.js";
+// import 判斷登入狀態樣板
+import isLoggedIn from '/assets/js/components/isLoggedIn.js';
+
+//全頁共用變數
+const _token = localStorage.getItem("token");
+
+
+/* 視登入狀態更改按鈕功能 ================================*/
+document.addEventListener('DOMContentLoaded', function () {
+    const btnCreateGrouping = document.querySelector(".btnCreateGrouping")
+    const btnCart = document.querySelectorAll(".btnCart")
+    const btnWriteReview = document.querySelectorAll(".btnWriteReview")
+
+
+    if (isLoggedIn(_token)) {
+        // 已登入，設置 modal 顯示目標
+        // 移除原本的 popover 屬性
+        btnCreateGrouping.setAttribute('href', '#modal-CreateGroup');
+        btnCreateGrouping.setAttribute("data-bs-toggle", "modal");
+
+        btnCart.forEach(btn => {
+            btn.setAttribute('href', '#modal-shppingCart-order');
+            btn.setAttribute("data-bs-toggle", "modal");
+        })        
+
+        btnWriteReview.forEach(btn => {
+            btn.setAttribute('href', '#modal-WriteReview');
+            btn.setAttribute("data-bs-toggle", "modal");
+        })
+
+    } else {
+        // 未登入，設置 popover
+        // 設置 popover 內容
+        btnCreateGrouping.setAttribute('data-bs-toggle', 'popover');
+        btnCreateGrouping.setAttribute('data-bs-content', '請先登入帳號');
+
+        btnCart.forEach(btn => {
+            btn.setAttribute('data-bs-toggle', 'popover');
+            btn.setAttribute('data-bs-content', '請先登入帳號');
+        })
+
+        btnWriteReview.forEach(btn => {
+            btn.setAttribute('data-bs-toggle', 'popover');
+            btn.setAttribute('data-bs-content', '請先登入帳號');
+        })
+
+        //popover 初始化
+        const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+        const popoverList = [...popoverTriggerList].map(popoverTriggerEl => {
+            new bootstrap.Popover(popoverTriggerEl)
+        })
+    }
+});
